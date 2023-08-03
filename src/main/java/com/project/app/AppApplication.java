2233abc233e23779cbc2e514ccb8a8d0a26b8db1
kosiyyu <kosiyyu@gmail.com>
2023-08-03 23:59:12 +0200

    add openapi

--- src/main/java/com/project/app/AppApplication.java
+++ src/main/java/com/project/app/AppApplication.java
@@ -1,7 +1,10 @@
 package com.project.app;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 public class AppApplication {
