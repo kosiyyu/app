e1d28c10d5ba18efd636fda052af50f13420b7fc
kosiyyu <kosiyyu@gmail.com>
2023-09-19 19:22:17 +0200

    csv import, fix tags

--- src/main/java/com/project/app/api/service/CsvService.java
+++ src/main/java/com/project/app/api/service/CsvService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Base64;
 import java.util.List;
@@ -24,25 +25,30 @@ public class CsvService {
         final BufferedReader bufferedReader = new BufferedReader(new StringReader(new String(bytes, StandardCharsets.UTF_8)));
         int index = 0; // csv iteration
         String line;
+        List<Tag> tags = null;
         while ((line = bufferedReader.readLine()) != null) {
             String[] arr = line.split(",(?=([^\"]*\"[^\"]*\")*[^\"]*$)");
             if(index == 0){
-                List<Tag> tags = Arrays.stream(Arrays.copyOfRange(arr, 9, arr.length))
+                tags = Arrays.stream(Arrays.copyOfRange(arr, 9, arr.length))
                         .map(Tag::new)
                         .toList();
-                tags.forEach(tagService::save);
+                tags = tagService.saveAll(tags);
             }
             else if(index == 1){
                 index++;
                 continue;
             }
             else {
-                List<Tag> localTags = Arrays.stream(Arrays.copyOfRange(arr, 9, arr.length))
-                        .filter(x -> x.equals("x"))
-                        .map(Tag::new)
+                List<String> values = Arrays.stream(Arrays.copyOfRange(arr, 9, arr.length))
                         .toList();
+                List<Tag> localTags = new ArrayList<>();
+                for(int i = 0; i < values.size(); i++){
+                    if(values.get(i).equals("x")){
+                        localTags.add(tags.get(i));
+                    }
+                }
                 Article article = new Article(arr[2], arr[3], arr[4], arr[5], arr[6], arr[7], Integer.parseInt(arr[8]), localTags);
-                articleService.save(article);
+                articleService.saveArticleWithUniqueTags(article);
             }
             index++;
         }
