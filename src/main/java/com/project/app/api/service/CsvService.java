b4909355c5ff243eb6f44ad7d98a80ecbde530f3
Karol Kos <kosiyyu@gmail.com>
2023-10-03 09:53:59 +0200

    s1 tbc

--- src/main/java/com/project/app/api/service/CsvService.java
+++ src/main/java/com/project/app/api/service/CsvService.java
@@ -6,10 +6,7 @@ import org.springframework.stereotype.Service;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Base64;
-import java.util.List;
+import java.util.*;
 
 @Service
 public class CsvService {
@@ -51,6 +48,84 @@ public class CsvService {
                 articleService.saveArticleWithUniqueTags(article);
             }
             index++;
+        }
+    }
+
+    public void loadCsv2(byte[] bytes) throws IOException {
+
+        final BufferedReader bufferedReader = new BufferedReader(new StringReader(new String(bytes, StandardCharsets.UTF_8)));
+        int lineIndex = 0;
+        String line = null;
+        char[] chars;
+
+        String[] values = new String[44];
+        Map<Integer, String> map = new HashMap<>();
+        while ((line = bufferedReader.readLine()) != null) {
+            if(lineIndex == 0){
+                lineIndex++;
+                int start = -1;
+                int end = -1;
+                int valueCounter = 0;
+                for(int i = line.length() - 1; i >= 0; i--) {
+                    if(line.charAt(i) == '"' && i == line.length() - 1){
+                        end = i;
+                    }
+                    else if(line.charAt(i) == '"' && line.charAt(i - 1) == ',' && line.charAt(i - 2) == '"') {
+                        start = i + 1;
+                        values[valueCounter] = line.substring(start, end);
+                        valueCounter++;
+                        end = i - 2;
+                        i -= 2;
+                    }
+                    else if(line.charAt(i) == ',' && line.charAt(i - 1) == '"'){
+                        // end
+                        start = i + 1;
+                        values[valueCounter] = line.substring(start, end);
+                        valueCounter++;
+                        end = i - 1;
+                    }
+                    else if(line.charAt(i) == ',' && line.charAt(i + 1) == '"'){
+                        // start
+                        start = i + 2;
+                        values[valueCounter] = line.substring(start, end);
+                        valueCounter++;
+                        end = i;
+                    }
+                    else if(line.charAt(i) == ','){
+
+                    }
+                }
+            }
+
+            char []charArray = line.toCharArray();
+            Vector<Integer> split = new Vector<>();
+            String[] stringArray = new String[5];
+
+
+            // 0 1 2            3         4         5            67         8
+            // 1,1,2D Materials,2053-1583,2053-1583,2D Materials,,2053-1583,140,,,,,,,,x,x,,x,,x,x,x,,x,,,,,,,,,,,,,,,,,,,,,,,,x,x,,
+            int wordCounter = 1; // initial is 0 line.indexOf(',') witch always will be a number
+            int splitAt = 0;
+            for(int i = line.indexOf(','); i < charArray.length; i++) {
+                if(charArray[i] == ',') {
+                    if(charArray[i + 1] == '"')
+                    {
+                        split.add(i + 2);
+                    }
+                    else if(charArray[i - 1] == '"')
+                    {
+                        split.add(i - 2);
+                    }
+                    else {
+                        split.add(i);
+                    }
+                }
+                if(charArray[i + 1] == '"')
+                {
+
+                }
+            }
+            lineIndex++;
         }
     }
 }
