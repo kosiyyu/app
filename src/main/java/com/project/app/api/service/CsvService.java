f53b40ca4a0b578dead9ba7ad75c181da083be16
Karol Kos <kosiyyu@gmail.com>
2023-10-04 04:28:03 +0200

    s3 tbc

--- src/main/java/com/project/app/api/service/CsvService.java
+++ src/main/java/com/project/app/api/service/CsvService.java
@@ -2,11 +2,13 @@ package com.project.app.api.service;
 
 import com.project.app.api.entity.Article;
 import com.project.app.api.entity.Tag;
+import org.apache.el.stream.Stream;
 import org.springframework.stereotype.Service;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.util.*;
+import java.util.stream.IntStream;
 
 @Service
 public class CsvService {
@@ -51,77 +53,105 @@ public class CsvService {
         }
     }
 
-    public void loadCsv2(byte[] bytes) throws IOException {
+    private static void extractValues(String line, String[] values, int numberOfWords) {
+        int start = -1;
+        int end = line.length();
+        int valueCounter = 0;
+        boolean isOpen = false;
+        for (int i = line.length() - 1; i >= 0; i--) {
+            if (line.charAt(i) == '"' && i == line.length() - 1) {
+                end = i;
+            } else if (line.charAt(i) == '"' && line.charAt(i - 1) == ',' && line.charAt(i - 2) == '"') {
+                values[valueCounter] = line.substring(i + 1, end);
+                valueCounter++;
+                end = i - 2;
+                isOpen = true;
+                i -= 2;
+            } else if (line.charAt(i) == ',' && line.charAt(i - 1) == '"') {
+                values[valueCounter] = line.substring(i + 1, end);
+                isOpen = true;
+                valueCounter++;
+                end = i - 1;
+            } else if (line.charAt(i) == ',' && line.charAt(i + 1) == '"') {
+                values[valueCounter] = line.substring(i + 2, end);
+                isOpen = false;
+                valueCounter++;
+                end = i;
+            } else if (line.charAt(i) == ',' && line.charAt(i - 1) != '"' && line.charAt(i + 1) != '"' && !isOpen) {
+                values[valueCounter] = line.substring(i + 1, end);
+                valueCounter++;
+                end = i;
+            } else if (i == 0) {
+                start = 0;
+                values[valueCounter] = line.substring(start, end);
+                valueCounter++;
+            }
+            if (valueCounter == numberOfWords) {
+                break;
+            }
+        }
+    }
 
+    private static void extractKeys(String line, int[] keys, int numberOfWords) {
+        int start = -1;
+        int end = line.length();
+        int valueCounter = 0;
+        boolean isOpen = false;
+        try {
+            for (int i = line.length() - 1; i >= 0; i--) {
+                if (line.charAt(i) == '"' && i == line.length() - 1) {
+                    end = i;
+                } else if (line.charAt(i) == '"' && line.charAt(i - 1) == ',' && line.charAt(i - 2) == '"') {
+                    keys[valueCounter] = Integer.parseInt(line.substring(i + 1, end));
+                    valueCounter++;
+                    end = i - 2;
+                    isOpen = true;
+                    i -= 2;
+                } else if (line.charAt(i) == ',' && line.charAt(i - 1) == '"') {
+                    keys[valueCounter] = Integer.parseInt(line.substring(i + 1, end));
+                    isOpen = true;
+                    valueCounter++;
+                    end = i - 1;
+                } else if (line.charAt(i) == ',' && line.charAt(i + 1) == '"') {
+                    keys[valueCounter] = Integer.parseInt(line.substring(i + 2, end));
+                    isOpen = false;
+                    valueCounter++;
+                    end = i;
+                } else if (line.charAt(i) == ',' && line.charAt(i - 1) != '"' && line.charAt(i + 1) != '"' && !isOpen) {
+                    keys[valueCounter] = Integer.parseInt(line.substring(i + 1, end));
+                    valueCounter++;
+                    end = i;
+                } else if (i == 0) {
+                    start = 0;
+                    keys[valueCounter] = Integer.parseInt(line.substring(start, end));
+                    valueCounter++;
+                }
+                if (valueCounter == numberOfWords) {
+                    break;
+                }
+            }
+        }
+        catch (Exception e){
+            throw new RuntimeException("err: " + e.getMessage() + " ," + valueCounter);
+        }
+    }
+
+    public void loadCsv2(byte[] bytes) throws IOException {
         final BufferedReader bufferedReader = new BufferedReader(new StringReader(new String(bytes, StandardCharsets.UTF_8)));
         int lineIndex = 0;
-        String line = null;
+        String line;
         int numberOfWords = 44;
-        Integer[] keys = new Integer[numberOfWords];
+        int[] keys = new int[numberOfWords];
         String[] values = new String[numberOfWords];
         Map<Integer, String> map = new HashMap<>();
         while ((line = bufferedReader.readLine()) != null) {
-            if(lineIndex == 0){
-                lineIndex++;
-                int start = -1;
-                int end = line.length();
-                int valueCounter = 0;
-                boolean isOpen = false;
-                for(int i = line.length() - 1; i >= 0; i--) {
-                    if(line.charAt(i) == '"' && i == line.length() - 1){
-                        end = i;
-                    }
-                    else if(line.charAt(i) == '"' && line.charAt(i - 1) == ',' && line.charAt(i - 2) == '"') {
-                        start = i + 1;
-                        values[valueCounter] = line.substring(start, end);
-                        valueCounter++;
-                        end = i - 2;
-                        isOpen = true;
-                        i -= 2;
-                    }
-                    else if(line.charAt(i) == ',' && line.charAt(i - 1) == '"'){
-                        // end
-                        start = i + 1;
-                        values[valueCounter] = line.substring(start, end);
-                        isOpen = true;
-                        valueCounter++;
-                        end = i - 1;
-                    }
-                    else if(line.charAt(i) == ',' && line.charAt(i + 1) == '"'){
-                        // start
-                        start = i + 2;
-                        values[valueCounter] = line.substring(start, end);
-                        isOpen = false;
-                        valueCounter++;
-                        end = i;
-                    }
-                    else if(line.charAt(i) == ',' && line.charAt(i - 1) != '"' && line.charAt(i + 1) != '"' && !isOpen){
-                        start = i + 1;
-                        values[valueCounter] = line.substring(start, end);
-                        isOpen = false;
-                        valueCounter++;
-                        end = i;
-                    }
-                    else if(i == 0){
-                        start = 0;
-                        values[valueCounter] = line.substring(start, end);
-                        isOpen = false; // doesn't matter
-                        valueCounter++;
-                    }
-
-                    if(valueCounter == numberOfWords){
-                        break;
-                    }
-                }
-                var a = "asdas";
+            // todo loops refactor: still
+            if (lineIndex == 0) {
+                extractValues(line, values, numberOfWords);
             }
-            // todo loop refactor
-
-            else if(lineIndex == 1){
-                // todo --> same loop for keys
-                for(int i = line.length() - 1; i >= 0; i--) {
-                    map.put(keys[i], values[i]);
-                }
+            else if(lineIndex == 1) {
+                extractKeys(line, keys, numberOfWords);
+                IntStream.range(0,numberOfWords - 1).forEach(x -> map.put(keys[x], values[x]));
             }
             else {
             }
