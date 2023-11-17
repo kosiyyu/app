0b9399ba7ae11dc4a39726d555c4997ea8a8535b
kosiyyu <kosiyyu@gmail.com>
2023-11-17 16:11:04 +0100

    add "update" and "deleteFile" methods

--- src/main/java/com/project/app/api/service/FileMetadataService.java
+++ src/main/java/com/project/app/api/service/FileMetadataService.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.NoSuchElementException;
 
 @Service
 public class FileMetadataService {
@@ -18,7 +20,7 @@ public class FileMetadataService {
         this.fileService = fileService;
     }
 
-    public void save(byte[] fileByteArray, String originalFilename) throws IOException {
+    public Metadata save(byte[] fileByteArray, String originalFilename) throws IOException {
         // SAVE METADATA TO DATABASE
         Metadata metadata = new Metadata(originalFilename, filesPath);
         metadata = metadataService.save(metadata);
@@ -27,5 +29,31 @@ public class FileMetadataService {
         String newFilename = metadata.getId() + extension;
         // SAVE FILE TO DIRECTORY
         fileService.save(fileByteArray, newFilename);
+        return metadata;
+    }
+
+    public Metadata update(Metadata metadata, byte[] fileByteArray, String originalFilename) throws NoSuchElementException, IOException {
+        // EXTRACT STRINGS
+        String newExtension = originalFilename.contains(".") ? originalFilename.substring(originalFilename.lastIndexOf(".")) : "";
+        String newFilename = metadata.getId() + newExtension;
+
+        String oldExtension = metadata.getOriginalFilename().contains(".") ? metadata.getOriginalFilename().substring(metadata.getOriginalFilename().lastIndexOf(".")) : "";
+        String oldFilename = metadata.getId() + oldExtension;
+        // UPDATE FILE TO DIRECTORY
+        fileService.update(oldFilename, fileByteArray, newFilename);
+        // UPDATE METADATA
+        metadata.setOriginalFilename(originalFilename);
+        metadata.setPath(filesPath);
+        return metadataService.save(metadata);
+    }
+
+    public void deleteFile(int id) throws NoSuchElementException, IOException {
+        Metadata metadata = metadataService.findById(id).orElseThrow();
+
+        String oldExtension = metadata.getOriginalFilename().contains(".") ? metadata.getOriginalFilename().substring(metadata.getOriginalFilename().lastIndexOf(".")) : "";
+        String oldFilename = metadata.getId() + oldExtension;
+
+        // DELETE FILE
+        fileService.delete(oldFilename);
     }
 }
