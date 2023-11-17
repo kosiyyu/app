b1342117fabb7bb44738f80a397c584a585cab59
kosiyyu <kosiyyu@gmail.com>
2023-11-17 16:44:25 +0100

    add "update" and "delete" methods to FileService

--- src/main/java/com/project/app/api/service/FileService.java
+++ src/main/java/com/project/app/api/service/FileService.java
@@ -1,8 +1,10 @@
 package com.project.app.api.service;
 
+import com.project.app.api.entity.Metadata;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
@@ -20,11 +22,30 @@ public class FileService {
         file.close();
     }
 
-    public void save(byte[] byteArray, String newFilename, String filePath) throws IOException {
-        String path = filePath + newFilename;
-        OutputStream file = new FileOutputStream(path);
-        file.write(byteArray);
-        file.flush();
-        file.close();
+    public void delete(String oldFilename) throws IOException {
+        String path = filePath + oldFilename;
+        File file = new File(path);
+        if(file.isFile()) {
+            if (!file.delete()) {
+                throw new IOException("Failed to delete a file.");
+            }
+        }
     }
+
+    public void update(String oldFilename, byte[] newByteArray, String newFilename) throws IOException {
+        String path = filePath + oldFilename;
+        File oldFile = new File(path);
+        if(oldFile.isFile()){
+            if(!oldFile.delete()) {
+                throw new IOException();
+            }
+        } else {
+            throw new IOException();
+        }
+        File newFile = new File(filePath + newFilename);
+        try (OutputStream outputStream = new FileOutputStream(newFile)) {
+            outputStream.write(newByteArray);
+        }
+    }
+
 }
