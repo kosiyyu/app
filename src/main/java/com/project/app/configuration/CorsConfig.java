8878f6ee943a4b9c8d32d8876696f38317d1246c
kosiyyu <kosiyyu@gmail.com>
2023-11-24 19:01:38 +0100

    update cors

--- src/main/java/com/project/app/configuration/CorsConfig.java
+++ src/main/java/com/project/app/configuration/CorsConfig.java
@@ -8,6 +8,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://localhost:5173").allowedMethods("*").allowedHeaders("*").allowCredentials(true);
+        registry
+                .addMapping("/**")
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .exposedHeaders("Content-Disposition")
+                .allowCredentials(true);
     }
 }
