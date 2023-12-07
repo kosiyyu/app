47ed05d609a5f00fc7b4967a1e3f06165e6647e7
kosiyyu <kosiyyu@gmail.com>
2023-12-07 02:52:59 +0100

    update

--- src/main/java/com/project/app/configuration/OpenApiConfig.java
+++ src/main/java/com/project/app/configuration/OpenApiConfig.java
@@ -6,6 +6,6 @@ import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
 
-@OpenAPIDefinition(info = @Info(title = "SPRING BOOT API", description = "TO DO", termsOfService = "????", contact = @Contact(name = "Karol Kos", email = "kosiyyu@gmail.com", url = "https://github.com/kosiyyu"), license = @License(), version = "1.0.0", extensions = @Extension(properties = {})))
+@OpenAPIDefinition(info = @Info(title = "scijourdex api", description = "This documentation provides endpoints for managing and retrieving data. Happy using.", termsOfService = "????", contact = @Contact(name = "Karol Kos", email = "kosiyyu@gmail.com", url = "https://github.com/kosiyyu/scijourdex-backend"), license = @License(), version = "1.0.0", extensions = @Extension(properties = {})))
 public class OpenApiConfig {
 }
